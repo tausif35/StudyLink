87281dc10c092602ad5ebebbd30d7d498c505fb0
saidul-baker <53114757+saidul-baker@users.noreply.github.com>
2020-11-03 03:11:34 +0600

    google

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
+const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+const findOrCreate =require("mongoose-findorcreate");
+
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}))
 const mongoose=require("mongoose");
@@ -39,10 +42,12 @@ const userSchema=new mongoose.Schema({
     email:String,
     password:String,
     fullname:String,
-    type:String
+    type:String,
+    googleId:String
 })
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 
 const User=new mongoose.model("User",userSchema)
@@ -50,8 +55,28 @@ const User=new mongoose.model("User",userSchema)
 passport.use(User.createStrategy());
 
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+passport.use(new GoogleStrategy({
+    clientID:     process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL :"http://www.googleapis.com/oauth2/v3/userInfo"
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
 
 app.get("/",function(req,res){
     if(req.isAuthenticated()){
@@ -60,6 +85,17 @@ app.get("/",function(req,res){
         res.redirect("/login")
     }
 })
+app.get('/auth/google',
+  passport.authenticate('google', { scope:
+      [ 'email', 'profile' ] }
+));
+
+app.get( '/auth/google/secrets',
+    passport.authenticate( 'google', {
+        successRedirect: '/home',
+        failureRedirect: '/login'
+}));
+
 app.get("/login",function(req,res){
     res.render("login")
 })
