97b4206ab3666815950bcdb909ae60cd42837ba5
Siam Ahmed <52734646+siam397@users.noreply.github.com>
2020-11-07 02:27:40 +0600

    login

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const bodyParser = require("body-parser");
 const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 const FacebookStrategy = require("passport-facebook").Strategy;
 const findOrCreate =require("mongoose-findorcreate");
-
+const multer  = require('multer')
+const path = require('path');
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}))
 const mongoose=require("mongoose");
@@ -13,10 +14,21 @@ const session = require('express-session');
 const passport=require("passport")
 const nodemailer = require("nodemailer");
 const generator = require('generate-password');
-
 const passportLocalMongoose=require("passport-local-mongoose");
 app.set("view engine","ejs");
 app.use(express.static("public"))
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'public/');
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+  
+const upload = multer({ storage: storage });
 app.use(session({
     secret:process.env.SECRET,
     resave:false,
@@ -48,12 +60,16 @@ const userSchema=new mongoose.Schema({
     facebookId:String
 })
 
+const questionSchema=new mongoose.Schema({
+    questions:[],
+})
+
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
 
 
 const User=new mongoose.model("User",userSchema)
-
+const Question=new mongoose.model("Question",questionSchema)
 passport.use(User.createStrategy());
 
 
@@ -91,13 +107,18 @@ passport.use(new FacebookStrategy({
   }
 ));
 
-app.get("/",function(req,res){
-    if(req.isAuthenticated()){
-        res.redirect("/home")
-    }else{
-        res.redirect("/login")
-    }
-})
+
+
+
+
+
+
+
+
+
+
+
+
 app.get('/auth/google',
   passport.authenticate('google', { scope:
       [ 'email', 'profile' ] }
@@ -108,8 +129,6 @@ app.get( '/auth/google/secrets',
         successRedirect: '/information',
         failureRedirect: '/login'
 }));
-
-
 app.get('/auth/facebook',
   passport.authenticate('facebook'));
 
@@ -119,6 +138,13 @@ app.get('/auth/facebook',
     // Successful authentication, redirect home.
     res.redirect('/home');
   });
+  app.get("/",function(req,res){
+    if(req.isAuthenticated()){
+        res.redirect("/home")
+    }else{
+        res.redirect("/login")
+    }
+})
 
 app.get("/information",function(req,res){
     if(req.isAuthenticated()){
@@ -129,15 +155,6 @@ app.get("/information",function(req,res){
         }
     }    
 })
-app.post("/information",function(req,res){
-    req.user.fullname=req.body.fullname;
-    req.user.username=req.body.username;
-    req.user.type=req.body.type;
-    req.user.save()
-    res.redirect("/home")
-})
-
-
 app.get("/login",function(req,res){
     res.render("login")
 })
@@ -151,7 +168,68 @@ app.get("/home",function(req,res){
         res.redirect("/login")
     }
 })
+app.get("/forgot",function(req,res){
+    res.render("forgot")
+})
+app.get("/question/:topic",function(req,res){
+    if(req.isAuthenticated() && req.user.type==="teacher"){
+        const id=req.params.topic;
+        Question.findById(id, function (err, qs) {
+            if(!qs){
+                res.send("wrong link.try again")
+            }else{
+                res.render("createQs",{topic:id})
+            }
+        })
+    }else{
+        if(req.user.type!=="teacher"){
+            res.send("tui hala chatro")
+        }else{
+            res.redirect("signup")
+        }
+    }
+})
+app.get("/questionpaper/:topic",function(req,res){
+    if(req.isAuthenticated()){
+        const id=req.params.topic;
+        Question.findById(id, function (err, qs) {
+            if(!qs){
+                res.send("wrong link.try again")
+            }else{
+                res.render("questionpaper",{qs:qs.questions})
+            }
+        })
+    }
+})
+app.post("/makeqs", upload.array('file', 12),function(req,res){
+    const id=req.body.id
+    // if(req.file){const obj={
+    //     originalName:req.file.originalname,
+    //     mimeType:req.file.mimetype,
+    //     size:req.file.size,
+    //     fileName:req.file.filename
+    // }}else{
+    //     obj=null;
+    // }
+    // const obj1={
+    //     question:req.body.qs,
+    //     file:obj,
+    //     mark:req.body.mark
+    // }
+    // if(req.body.btn==="add"){
+    //     Question.findById(id, function (err, qs) {
+    //         if(err){
+    //             res.send("err")
+    //         }else{
+    //             qs.questions.push(obj1)
+    //             qs.save()
+    //             res.redirect("/questionPaper/"+id)
+    //         }
+    //     })
+    // }
 
+    console.log(req)
+})
 
 app.post("/signup",function(req,res){
     User.register({username:req.body.username,email:req.body.email,type:req.body.type,fullname:req.body.fullname}, req.body.password, function(err,user){
@@ -165,8 +243,14 @@ app.post("/signup",function(req,res){
         }
     })    
 })
-app.get("/forgot",function(req,res){
-    res.render("forgot")
+
+
+app.post("/information",function(req,res){
+    req.user.fullname=req.body.fullname;
+    req.user.username=req.body.username;
+    req.user.type=req.body.type;
+    req.user.save()
+    res.redirect("/home")
 })
 
 app.post("/forgot",function(req,res){
@@ -254,6 +338,22 @@ app.post("/logout",function(req,res){
     req.logout();
     res.redirect("/login");
 })
+
+app.post("/redirect",function(req,res){
+    if(req.body.btn==="makeqs"){
+        Question.create({ questions:[] }, function (err, qs) {
+            if (err) return handleError(err);
+            else{
+                qs.save()
+                res.redirect("/question/"+qs.id)
+            }
+          });
+    }
+})
+
+
+
+
 
 app.listen("3000",function(err){
     if(err){
